a2e442a11146abe35b6aa05dd76d1b0c474b2f97
Loredan <davidloredanvicentiu@gmail.com>
2021-07-22 10:12:26 +0200

    added about me components and contact form components

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -4,7 +4,19 @@ import SAboutMe from './style';
 function AboutMe() {
   return (
     <SAboutMe>
-      <div>description</div>
+      <div>
+        <img className="headerStyle" src="/image/bg.jpg" alt="logo" />
+      </div>
+      <section className="descriptionStyle">
+        <p className="textImage">
+          Loredan DAVID Développeur web, spécialisé dans le JavaScript et les
+          frameworks React /vue.Js, parfaitement à l’aise avec le HTML / CSS /
+          JavaScript et le développement d’application mobile React Native.
+          Passionné du monde numérique, autodidacte et toujours à l&apos;affût
+          de l’actu web. J’aime lire, me tenir informé des dernières nouveautés
+          mobiles / Android.
+        </p>
+      </section>
     </SAboutMe>
   );
 }
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,51 @@
 import SContactMe from './style';
 
 function ContactMe() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <SContactMe>
-      <div />
+      <div className="content">
+        <h2 className="titleContact">Contact Me</h2>
+
+        <form className="contactUs" onSubmit={handleSubmit}>
+          <label htmlFor="username">
+            <h3>Votre Nom et Prenom</h3>
+            <input
+              className="nomsStyle"
+              placeholder="Nom"
+              name="firstname"
+              id="field"
+            />
+          </label>
+
+          <label htmlFor="email">
+            <h3>Email</h3>
+            <input
+              className="emailStyle"
+              placeholder="votre@mail.com"
+              name="email"
+              id="field"
+            />
+          </label>
+
+          <label htmlFor="message">
+            <h3>Message</h3>
+            <textarea
+              className="text"
+              type="textarea"
+              name="textValue"
+              id="field"
+              placeholder="Votre message"
+            />
+          </label>
+
+          <button type="submit" className="button">
+            <h4>Envoyé</h4>
+          </button>
+        </form>
+      </div>
     </SContactMe>
   );
 }
