890bf3ed14d9ffee60a48bb79f8618d67447a275
Loredan <davidloredanvicentiu@gmail.com>
2021-07-22 16:20:15 +0200

    added curriculum Vitae components  contact form  css

--- src/components/AboutMe/style.jsx
+++ src/components/AboutMe/style.jsx
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 const SAboutMe = styled.div`
   .headerStyle {
     height: 40vh;
-    width: 100vw;
+    width: 99vw;
     object-fit: none;
   }
   .textImage {
-    color: black;
+    color: #8e9dae;
     margin-left: 10vw;
     margin-right: 10vw;
     margin-top: 50px;
     font-size: 2em;
+    text-align: justify;
   }
 `;
 
